e64dbaf018886796a6754c7e4680c96abbc866bf
AlexXG0152 <34213814+AlexXG0152@users.noreply.github.com>
2022-01-08 16:21:46 +0300

    solve something

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -65,8 +65,12 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const name = value.split(' ').slice(-2);
+  name.forEach((i) => {
+    i.replace('!', '');
+  });
+  return name.join(' ');
 }
 
 
